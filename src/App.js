8b4483ceb424f96d263fffc1aa811f21613d5d48
naiden <na4ko359@gmail.com>
2022-05-08 11:02:13 +0300

    fetch random quote

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import style from "./style/style.css";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [quote, setQuote] = useState("");
+
+  function getRandomQuote() {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(data => setQuote(data))
+      console.log(quote);
+  }
+
+  React.useEffect(() => {
+    getRandomQuote();
+  }, []);
+
   return (
     <div className="App">
-      <p>hey</p>
+      <p>{quote.content}</p>
+      <p>{quote.author}</p>
+      <button onClick={getRandomQuote}>Click</button>
     </div>
   );
 }
